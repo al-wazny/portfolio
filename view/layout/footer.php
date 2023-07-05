63bf93adce21c707670898658fd2143ff1902cdb
ali.al-wazny <ali.al-wazny@flagbit.de>
2023-07-05 16:34:48 +0200

    fix: Forgot to add the js to make it actually work

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -41,5 +41,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.2.0/js/swiper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <?php } ?>
