5a6fe7afa4951319d8da18f6af9889a725c78f7a
Ali Al-Wazny <ali@pop-os.localdomain>
2023-07-20 09:47:27 +0200

    release v0.2.2

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <meta name="description" content="An Agency Specialized in web development from Karlsruhe providing custom tailored software solutions">
     <meta name="keywords" content="karlsruhe, software, agency, agentur, websites, webseiten, website, automation, hosting, custom, software solutions, web entwicklung, web, dev, development, high-quality service, responsive support, personalized support, modern technologie">
     <meta name="author" content="Ali Al-Wazny">
-    <!-- <link rel="canonical" href="http://www.wishfulgoods.eu/index.php"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
